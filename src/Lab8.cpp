fb2ebbc6fc711d494b86255d9130eb15955216c8
ShoubhitP <spusulur24@gmail.com>
2023-07-12 13:27:35 -0500

    hi

---
+++ src/Lab8.cpp
@@ -0,0 +1,113 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/shoubhitsstuff/Desktop/Lab8/src/Lab8.ino"
+void setup();
+void loop();
+void refreshDisplay();
+void printProximity();
+void printAmbientLight();
+#line 1 "/Users/shoubhitsstuff/Desktop/Lab8/src/Lab8.ino"
+SYSTEM_THREAD(ENABLED);
+
+#include "Wire.h"
+#include "blynk.h"
+#include "SparkFun_VCNL4040_Arduino_Library.h"
+#include "oled-wing-adafruit.h"
+
+#define BLYNK_TEMPLATE_ID "TMPL2EKRGok8d"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "I6qMbq7oS9Qbn2gEUzw4G7kuT7kHQT-v"
+
+OledWingAdafruit display;
+VCNL4040 proximitySensor;
+uint16_t proximityValue;
+int lightValue;
+
+void setup() {
+
+display.setup();
+Serial.begin(9600);
+Wire.begin();
+
+
+proximitySensor.begin();
+proximitySensor.powerOnProximity();
+proximitySensor.powerOnAmbient();
+proximitySensor.enableWhiteChannel();
+
+pinMode(D5, OUTPUT);
+pinMode(D6, OUTPUT);
+pinMode(D7, OUTPUT);
+
+refreshDisplay();
+display.println("Starting data reading..");
+display.display();
+}
+void loop() {
+
+display.loop();
+
+proximityValue = proximitySensor.getProximity();
+lightValue = proximitySensor.getAmbient();
+
+if(proximityValue <= 50)
+{
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, LOW);
+}
+else if(proximityValue <= 150)
+{
+
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, HIGH);
+  digitalWrite(D7, LOW);
+}
+else
+{
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, HIGH);
+}
+
+if(display.pressedA())
+{
+    refreshDisplay();
+    printProximity();
+    
+}
+if(display.pressedB())
+{
+   refreshDisplay(); 
+   printAmbientLight();
+}
+if(display.pressedC())
+{
+
+
+}
+
+}
+void refreshDisplay()
+{
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+}
+
+void printProximity()
+{
+  display.print("Current proximity results: ");
+  display.print(proximityValue);
+  display.display();
+}
+void printAmbientLight()
+{
+  display.print("Current light level: ");
+  display.print(lightValue);
+  display.display();
+}
---
+++ target/4.1.0/argon/obj/src/newlib_stubs.o.d
@@ -0,0 +1,105 @@
+/Users/shoubhitsstuff/Desktop/Lab8/target/4.1.0/argon//obj/./src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ ../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/types.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/cdefs.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_types.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_types.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/endian.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_endian.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/select.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_sigset.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timeval.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/timespec.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timespec.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/sched.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/types.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/stdlib.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/cstdlib \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/pstl/pstl_config.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdlib.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/reent.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/lock.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/stdlib.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/alloca.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_locale.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/bits/std_abs.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/string.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/strings.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/string.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdarg.h \
+ ../../../services/inc/panic.h ../../../services/inc/config.h \
+ ../../../services/inc/preprocessor.h ../../../hal/shared/platforms.h
+../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/types.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/cdefs.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_types.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_types.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/endian.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_endian.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/select.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_sigset.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timeval.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/timespec.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timespec.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_pthreadtypes.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/sched.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/types.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/stdlib.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/cstdlib:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/pstl/pstl_config.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdlib.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/reent.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/lock.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/stdlib.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/alloca.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_locale.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/bits/std_abs.h:
+../../../services/inc/debug.h:
+../../../services/inc/service_debug.h:
+../../../services/inc/logging.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/string.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/strings.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/string.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h:
+/Users/shoubhitsstuff/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdarg.h:
+../../../services/inc/panic.h:
+../../../services/inc/config.h:
+../../../services/inc/preprocessor.h:
+../../../hal/shared/platforms.h:
